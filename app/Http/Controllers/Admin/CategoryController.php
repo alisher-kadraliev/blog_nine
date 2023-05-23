16031be4e872785f29cd9460b0073747782df633
Alisher Kadraliev <alisherkadraliev@gmail.com>
2023-05-23 18:36:36 +0300

    start second turn

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -3,12 +3,27 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreRequest;
+use App\Models\Category;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
-        return view('admin.category.index');
+        $categories = Category::all();
+        return view('admin.category.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('admin.category.create');
+    }
+
+    public function store(StoreRequest $request)
+    {
+        $data = $request->validated();
+        Category::firstOrCreate($data);
+        return redirect()->route('admin.category.index');
     }
 }
